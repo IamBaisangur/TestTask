cded9b5f04b9c5b46b7ab0b58ff4d9bddc96992b
Baisangur <baisangur.ozdoev@gmail.com>
2022-03-12 14:13:21 +0300

    initial

---
+++ TestTask/MainViewController.swift
@@ -0,0 +1,50 @@
+//
+//  MainViewController.swift
+//  TestTask
+//
+//  Created by Байсангур on 12.03.2022.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+    let notesName = ["notesName1","notesName2","notesName3","notesName4"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+  
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notesName.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel?.text = notesName[indexPath.row]
+
+        return cell
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
